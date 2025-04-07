144867ebf8c9ebfc46a2376ba0a8a5af0d5f957f
cbepx <62635246+cbepx-me@users.noreply.github.com>
2025-04-07 10:46:18 +0800

    Update Image_Browser.sh

--- Image_Browser/Image_Browser.sh
+++ Image_Browser/Image_Browser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 . /mnt/mod/ctrl/configs/functions &>/dev/null 2>&1
-progdir="$(cd $(dirname "$0"); pwd)"/img_browser
+progdir="$(cd $(dirname "$0") || exit; pwd)"/img_browser
 
 program="python3 ${progdir}/main.py"
 log_file="${progdir}/log.txt"
