6a817209e3836c87dccb9f2029aaca9269676821
cbepx <62635246+cbepx-me@users.noreply.github.com>
2025-05-16 10:48:52 +0800

    Update main.py
    Add identification code for new model.

--- Port_Master/port_master/main.py
+++ Port_Master/port_master/main.py
@@ -4,19 +4,21 @@ from pathlib import Path
 board_mapping = {
     'RGcubexx': 1,
     'RG34xx': 2,
+    'RG34xxSP': 2,
     'RG28xx': 3,
     'RG35xx+_P': 4,
     'RG35xxH': 5,
     'RG35xxSP': 6,
     'RG40xxH': 7,
-    'RG40xxV': 8
+    'RG40xxV': 8,
+    'RG35xxPRO': 9
 }
 system_list = ['zh_CN', 'zh_TW', 'en_US', 'ja_JP', 'ko_KR', 'es_LA', 'ru_RU', 'de_DE', 'fr_FR', 'pt_BR']
 
 try:
     board_info = Path("/mnt/vendor/oem/board.ini").read_text().splitlines()[0]
 except (FileNotFoundError, IndexError):
-    board_info = ''
+    board_info = 'RG35xxH'
     
 try:
     lang_info = Path("/mnt/vendor/oem/language.ini").read_text().splitlines()[0]
